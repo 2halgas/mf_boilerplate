87866f7162cc405895dddcd5baae130490d87ef1
2halgas <khamidulla.2halgas@gmail.com>
2024-10-14 16:24:04 +0500

    feat: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,8 @@ const { configs: typescriptEslintConfigs } = typescriptEslintPkg;
 import importPlugin from "eslint-plugin-import";
 import parser from "@typescript-eslint/parser";
 import globals from "globals";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
@@ -29,13 +31,14 @@ export default defineConfig([
       react: pkg,
       import: importPlugin,
       "@typescript-eslint": typescriptEslintPkg,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       // Use valid import rules
       "import/named": "error",
       "import/default": "error",
       "import/namespace": "error",
-      "import/no-unresolved": "error",
+      "import/no-unresolved": "off",
       "import/no-extraneous-dependencies": "warn",
       "import/export": "error",
       "import/no-duplicates": "warn",
@@ -47,6 +50,9 @@ export default defineConfig([
       "react/react-in-jsx-scope": "off",
       "no-console": "warn", // Example rule
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // Example rule
+      "@typescript-eslint/no-explicit-any": "warn",
+      "prettier/prettier": "error",
+      ...eslintConfigPrettier.rules,
     },
     settings: {
       react: {
