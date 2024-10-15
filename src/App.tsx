a7ef2c25df4a797de01963d27932dd2f4d6b228c
2halgas <khamidulla.2halgas@gmail.com>
2024-10-15 11:27:53 +0500

    WIP: localization

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,29 @@ import ReactDOM from "react-dom/client";
 
 import "./index.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Providers from "./components/Providers";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: EFO_core_app</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+import "./i18n";
+import { useTranslation } from "react-i18next";
+
+const App = () => {
+  const { t, i18n } = useTranslation("translation");
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: EFO_core_app</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+      <div>{t("auth")}</div>
+      <select onChange={(e) => i18n.changeLanguage(e.target.value)}>
+        <option>Choose language</option>
+        <option value="kz">Kazakh</option>
+        <option value="ru">Russian</option>
+        <option value="en">English</option>
+      </select>
+    </div>
+  );
+};
 
 const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 const rootElement = document.getElementById("app");
