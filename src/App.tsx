a73cdecab2da566a253cf2ae4443cf9a6228e4e4
2halgas <khamidulla.2halgas@gmail.com>
2024-11-01 10:48:54 +0500

    Revert "test"
    This reverts commit c8eafd8481a7a9c19ee365177c7fd2a8010f100a.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Providers from "./components/Providers";
 
 import "./i18n";
-import AuthPage from "./pages/Auth";
 
-const router = createBrowserRouter([{ path: "/", element: <AuthPage /> }]);
+const App = () => {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl font-inter">
+      <div className="text-red-200">Name: EFO_core_app</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 
